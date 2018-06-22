32ae1e00d3fcce501c4bfda5e0fb62e71fc532a7
Roderic Page <rdmpage@gmail.com>
2018-06-22 13:04:48 +0100

    Improve name cleaning

--- parse_authors.php
+++ parse_authors.php
@@ -2,16 +2,57 @@
 
 // Parse author name string
 
+// latinse
+
+//----------------------------------------------------------------------------------------
+// https://stackoverflow.com/a/1454409/9684
+function strtr_utf8($str, $from, $to) {
+    $keys = array();
+    $values = array();
+    preg_match_all('/./u', $from, $keys);
+    preg_match_all('/./u', $to, $values);
+    $mapping = array_combine($keys[0], $values[0]);
+    return strtr($str, $mapping);
+}
 
 //----------------------------------------------------------------------------------------
 function name_to_id ($name)
 {
+	//echo $name . "\n";
+	
 	$name = str_replace('.', '', $name);
-	$name = strtolower($name);
+	$name = str_replace('´', '', $name);	
+	$name = str_replace('­', '', $name);
+	
+	$name = preg_replace('/[\x00-\x1F\x7F]/', '', $name);	
+	
+	$name = strip_tags($name);
+	
+	
+	//echo $name . "\n";
+	
+	$name = mb_convert_case($name, MB_CASE_LOWER);
+	
+	//echo $name . "\n";
+	
+	/*
+	$name = strtr(utf8_decode($name), 
+		utf8_decode("ÀÁÂÃÄÅàáâãäåæĀāĂăĄąÇçĆćĈĉĊċČčÐðĎďĐđÈÉÊËèéêëĒēĔĕĖėĘęĚěĜĝĞğĠġĢģĤĥĦħÌÍÎÏìíîïĨĩĪīĬĭĮįİıĴĵĶķĸĹĺĻļĽľĿŀŁłÑñŃńŅņŇňŉŊŋÒÓÔÕÖØòóôõöøŌōŎŏŐőŔŕŖŗŘřŚśŜŝŞşŠšſŢţŤťŦŧÙÚÛÜùúûüŨũŪūŬŭŮůŰűŲųŴŵÝýÿŶŷŸŹźŻżŽž"),
+		"aaaaaaaaaaaaaaaaaaaccccccccccddddddeeeeeeeeeeeeeeeeeegggggggghhhhiiiiiiiiiiiiiiiiiijjkkkllllllllllnnnnnnnnnnnoooooooooooooooooorrrrrrsssssssssttttttuuuuuuuuuuuuuuuuuuuuwwyyyyyyzzzzzz");
+	*/
+	
+	$name = strtr_utf8($name, 
+		"ÀÁÂÃÄÅàáâãäåæĀāĂăĄąÇçĆćĈĉĊċČčÐðĎďĐđÈÉÊËèéêëĒēĔĕĖėĘęĚěĜĝĞğĠġĢģĤĥĦħÌÍÎÏìíîïĨĩĪīĬĭĮįİıĴĵĶķĸĹĺĻļĽľĿŀŁłÑñŃńŅņŇňŉŊŋÒÓÔÕÖØòóôõöøŌōŎŏŐőŔŕŖŗŘřŚśŜŝŞşŠšſŢţŤťŦŧÙÚÛÜùúûüŨũŪūŬŭŮůŰűŲųŴŵÝýÿŶŷŸŹźŻżŽž",
+		"aaaaaaaaaaaaaaaaaaaccccccccccddddddeeeeeeeeeeeeeeeeeegggggggghhhhiiiiiiiiiiiiiiiiiijjkkkllllllllllnnnnnnnnnnnoooooooooooooooooorrrrrrsssssssssttttttuuuuuuuuuuuuuuuuuuuuwwyyyyyyzzzzzz");
+
+	//echo $name . "\n";
 
 	$parts = explode(' ', $name);
 
 	$id = join('-', $parts);
+	
+	//echo $name . $id . "\n";
+	//exit();
 
 	return $id;
 }
@@ -25,19 +66,19 @@ function parse_authors($authorstring)
 	//echo $authorstring . "\n";
 	
 	// ignore Jr (it's a hassle)
-	$authorstring = preg_replace('/\.\s+Jr\.?/', '.', $authorstring);
+	$authorstring = preg_replace('/\.\s+Jr\.?/u', '.', $authorstring);
 	
 	// remove et al.
 	$authorstring = preg_replace('/,?\s+et al./', '', $authorstring);
 	
 	// trim authors before ' in ' because we want authors of work, not name
-	$authorstring = preg_replace('/^(.*)\s+in\s+/', '', $authorstring);		
+	$authorstring = preg_replace('/^(.*)\s+in\s+/u', '', $authorstring);		
 
-	$authorstring = preg_replace('/\.\s*,\s+([^&])/', '.|$1', $authorstring);
+	$authorstring = preg_replace('/\.\s*,\s+([^&])/u', '.|$1', $authorstring);
 	
 	$authorstring = preg_replace('/,? & /', '|', $authorstring);
-	$authorstring = preg_replace('/([A-Z])\.([A-Z])/', '$1. $2', $authorstring);
-	$authorstring = preg_replace('/([A-Z])\.([A-Z])/', '$1. $2', $authorstring);
+	$authorstring = preg_replace('/([A-Z])\.([A-Z])/u', '$1. $2', $authorstring);
+	$authorstring = preg_replace('/([A-Z])\.([A-Z])/u', '$1. $2', $authorstring);
 	
 	//echo $authorstring . "\n";
 
@@ -55,6 +96,8 @@ function parse_authors($authorstring)
 			$a->name 		= $parts[1] . ' ' . $parts[0];
 			$a->givenName 	= $parts[1];
 			$a->familyName 	= $parts[0];
+			
+			//print_r($a);
 			
 			$a->id = name_to_id($a->name);
 			
